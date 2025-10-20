f8f7167fb76c4f75e19e0edff6c9a3f73766654e
yyh030806 <3279325045@qq.com>
2025-10-20 17:02:14 +0800

    add energy scorer

---
+++ ts_rl/energy_scorer.py
@@ -0,0 +1,19 @@
+from reactot.analyze.geomopt import calc_deltaE
+
+
+class EnergyScorer:
+    def __init__(self):
+        pass
+    
+    def __call__(self, st_tar, st_pred):
+        deltaE = calc_deltaE(st_tar, st_pred)
+        return deltaE
+        
+if __name__ == '__main__':
+    st_tar = 'H 0 0 0; Cl 0 0 1'
+    st_pred = 'H 0 0 0; F 0 0 1'
+    
+    scorer = EnergyScorer()
+    score = scorer(st_tar, st_pred)
+    
+    print(score)
